e533a0d5ddd8ff9ba3e39928253e1f8738493ee6
MeralSavas <meralsavas@gmail.com>
2022-11-30 17:53:59 +0000

    todos

---
+++ src/components/TodoListItem.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TodoListItem = () => {
+  return (
+    <li>
+      <p className="checked">Todo </p>
+      <p> Todo </p>
+      <span className="task-icons">✖️</span>
+    </li>
+  );
+};
+
+export default TodoListItem;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,39 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import InputForm from "../components/InputForm";
+import TodoList from "../components/TodoList";
+
+const url = "https://6352dcdca9f3f34c374a7bbe.mockapi.io/todos";
+
+interface TodoType {
+  id: string | number;
+  task: string;
+  isDone: boolean;
+}
+
+const Home = () => {
+  const [todos, setTodos] = useState<TodoType[]>([]);
+
+  const getTodos = async () => {
+    try {
+      const { data } = await axios.get<TodoType[]>(url);
+      console.log(data);
+      setTodos(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  return (
+    <div className="main">
+      <InputForm />
+      <TodoList todos={todos} />
+    </div>
+  );
+};
+
+export default Home;
